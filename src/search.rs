4c08edc99618490ad191505fa4ec669912d50197
Felix Berman <felix@berman.id.au>
2024-05-20 13:28:16 +1000

    Remove redundant pv array from SearchInfo

--- src/search.rs
+++ src/search.rs
@@ -51,7 +51,6 @@ struct SearchInfo {
     nodes: u32,
     stop_nodes: u32,
     time: Instant,
-    pv: [Option<Move>; MAX_DEPTH],
     triangular_pv: [Option<Move>; PV_SIZE], 
     history: [u64; MAX_GAME_PLY],
     tx: Sender<SendInfo>,
@@ -68,7 +67,6 @@ impl SearchInfo {
             nodes: 0,
             stop_nodes,
             time: Instant::now(),
-            pv: [None; MAX_DEPTH],
             triangular_pv: [None; PV_SIZE],
             history,
             tx,
@@ -84,7 +82,7 @@ impl SearchInfo {
             score: self.score,
             nodes: self.nodes,
             time: self.time.elapsed().as_millis() as u32,
-            pv: self.pv,
+            pv: self.triangular_pv[0..MAX_DEPTH].try_into().unwrap(),
         };
 
         self.tx.send(SendInfo::Full(full))
@@ -125,8 +123,6 @@ pub fn iterative_deepening(
         info.nodes = 0;
         info.score = negamax(&mut pos, -i32::MAX, i32::MAX, depth, 0, 0, &mut info);
 
-        info.pv = info.triangular_pv[0..MAX_DEPTH].try_into().unwrap();
-
         info.send_full().unwrap();
 
         if let Ok(SearchCommand::Stop) = info.rx.try_recv() {
@@ -146,11 +142,6 @@ pub fn iterative_deepening(
 }
 
 fn negamax(pos: &mut Position, mut alpha: i32, beta: i32, depth: u8, ply: usize, pv_idx: usize, info: &mut SearchInfo) -> i32 {
-    if let Ok(SearchCommand::Stop) = info.rx.try_recv() {
-        info.stop = true;
-        return alpha
-    }
-
     if ply as u8 > info.seldepth {
         info.seldepth = ply as u8;
     }
@@ -163,7 +154,7 @@ fn negamax(pos: &mut Position, mut alpha: i32, beta: i32, depth: u8, ply: usize,
         return quiescence_search(pos, alpha, beta, ply, info);
     }
     
-    info.triangular_pv[pv_idx] = None;
+    // info.triangular_pv[pv_idx] = None;
     let next_pv_idx = pv_idx + MAX_DEPTH - ply; 
 
     let mut moves = Vec::new();
@@ -189,17 +180,24 @@ fn negamax(pos: &mut Position, mut alpha: i32, beta: i32, depth: u8, ply: usize,
         let score = -negamax(pos, -beta, -alpha, depth - 1, ply + 1, next_pv_idx, info);
         info.nodes += 1;
 
-        if info.nodes > info.stop_nodes || info.stop {
-            return alpha
-        }
-        
         if score >= beta {
             return beta
         }
+
         if score > alpha {
             alpha = score;
             info.triangular_pv[pv_idx] = Some(mv);
             info.hoist_pv(pv_idx + 1, next_pv_idx, MAX_DEPTH - ply - 1);
+        }
+
+        if let Ok(SearchCommand::Stop) = info.rx.try_recv() {
+            info.stop = true;
+            return alpha
+        }
+
+        if info.rx.try_recv().is_ok_and(|cmd| matches!(cmd, SearchCommand::Stop))
+        || info.nodes > info.stop_nodes || info.stop {
+            return alpha
         }
 
         *pos = prev;
@@ -226,7 +224,7 @@ fn quiescence_search(pos: &mut Position, mut alpha: i32, beta: i32, ply: usize, 
         return beta
     }
 
-    if alpha < standing_pat {
+    if standing_pat > alpha {
         alpha = standing_pat;
     }
 
@@ -272,7 +270,7 @@ fn detect_repetition(pos: &Position, history: [u64; MAX_GAME_PLY]) -> bool {
 
 fn _score_moves(moves: &mut Vec<Move>, ply: usize, info: &SearchInfo) {
 
-    if let Some(pv_mv) = info.pv[ply] {
+    if let Some(pv_mv) = info.triangular_pv[ply] {
         for mv in moves {
             if *mv == pv_mv {
                 mv.sort_score += 100;
@@ -286,7 +284,7 @@ fn next_move(moves: &mut Vec<Move>, current: usize, info: &SearchInfo, ply: usiz
     let mut next_best = moves[current];
 
     for i in (current + 1)..moves.len() {
-        if  info.pv[ply].is_some_and(|mv| mv == moves[i]) {
+        if  info.triangular_pv[ply].is_some_and(|mv| mv == moves[i]) {
             next_best = moves[i];
             moves[i] = moves[current];
             moves[current] = next_best;
